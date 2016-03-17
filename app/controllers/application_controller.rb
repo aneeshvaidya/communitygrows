8a3af70539f51ef4587de3f9a8bb2a08d93b0cb1
Tony (Yoon Ho) Lee <tonylee1993@gmail.com>
2016-03-17 00:49:08 +0000

    redirecting after authentication and flash[:notice] and flash[:alert] implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,21 @@ class ApplicationController < ActionController::Base
   layout "application"
   
   # by Tony
+  # redirecting to the dashboard after authentication
+  def after_sign_in_path_for(resource)
+    user_id = current_user.id
+    flash[:message] = "Welcome back #{user_id}"
+    redirect_to('/dashboard#index')
+  end  
+  
+  # by Tony
+  # redirecting a new session to a sign in
+  # if logged in, redirect to the dash board
   def authenticate
     if user_signed_in?
-      flash[:message] = "Welcome back!"
-      
+      user_id = current_user.id
+      flash[:message] = "Welcome back #{user_id}"
+      redirect_to("/dashboard#index")
     else
       flash[:message] = "Please sign in"
       redirect_to("/users/sign_in")
