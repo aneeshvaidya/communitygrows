4b34794de5afddbde1fcbed8ddc7baacbbd7dbe8
aneeshvaidya <aneesh.h.vaidya@gmail.com>
2016-03-18 03:20:13 +0000

    Added simplecov to spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+SimpleCov.start 'rails'
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 RSpec.configure do |config|
