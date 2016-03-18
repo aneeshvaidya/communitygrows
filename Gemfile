4492caece019a73d6f07440ecc7ebd8844ce2335
Tony (Yoon Ho) Lee <tonylee1993@gmail.com>
2016-03-18 04:48:35 +0000

    rspec over 90%

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ group :development do
   gem 'web-console', '~> 2.0'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'rspec-rails'
 end
 
 group :development, :test do
---
+++ spec/application_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+require "rails_helper"
+
+RSpec.describe "User security", :type => :request do
+  describe "CommunityGrows website" do
+    it 'should redirect to the login unless the user is logged in' do
+      get '/'
+      response.should redirect_to('/users/sign_in')
+    end
+    it 'should redirect to the login when unsigned user is trying to access the dashboard' do
+      get '/dashboard'
+      response.should redirect_to('/users/sign_in')
+    end
+    it 'should redirect to the login when unsigned user is trying to access the subcommittees' do
+      get '/committee'
+      response.should redirect_to('/users/sign_in')
+    end
+    it 'should redirect to the login when unsigned user is trying to access the documents' do
+      get '/documents'
+      response.should redirect_to('/users/sign_in')
+    end
+    it 'should redirect to the login when unsigned user is trying to access the admin' do
+      get '/admin'
+      response.should redirect_to('/users/sign_in')
+    end
+    it 'should redirect to the dashboard when user is signed in' do
+      curr = User.create!(:email => "admin@communitygrows.org", :password => "communitygrowsrocks", :password_confirmation => "communitygrowsrocks", :admin => true)
+      visit "/users/sign_in"
+      fill_in "Email",    :with => curr.email
+      fill_in "Password", :with => curr.password
+      click_button "Log in"
+      page.should have_content("Dashboard")
+      
+      # then go back to the root again
+      visit "/"
+      page.should have_content("Dashboard")
+    end
+    
+  end
+end
