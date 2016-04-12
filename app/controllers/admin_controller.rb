a723657c4ebab5739806cb852c9a6b856e9971c9
Zachary T. McPherson <zmcpher@berkeley.edu>
2016-04-12 01:56:17 +0000

    Finished displaying user create and update errors

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -18,6 +18,7 @@ class AdminController < ActionController::Base
         @users = User.all
         @announcement_list = Announcement.order(created_at: :DESC)
         if !current_user.admin
+            flash[:message] = "Access not granted. Please sign in again."
             redirect_to("/users/sign_in")
         end
     end
@@ -31,7 +32,7 @@ class AdminController < ActionController::Base
         begin
             @user.update_attributes!(user_params)
         rescue Exception
-            flash[:notice] = "Populate all fields before submission."
+            flash[:notice] = flash[:notice].to_a.concat @user.errors.full_messages
             redirect_to edit_user_path(@user.id)
         else
             flash[:notice] = "#{@user.email} was successfully updated."
@@ -44,9 +45,10 @@ class AdminController < ActionController::Base
     def create_user
         #try and catch
         begin
-            @user = User.create!(user_params)
+            @user = User.create(user_params)
+            @user.save!
         rescue Exception => e
-            flash[:notice] = "Populate all fields before submission."
+            flash[:notice] = flash[:notice].to_a.concat @user.errors.full_messages
             redirect_to new_user_path
         else
             flash[:notice] = "#{@user.email} was successfully created."
--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,6 @@
 module AdminHelper
+    def devise_error_messages!
+        return "" if resource.errors.empty?
+        return resource.errors
+    end
 end
