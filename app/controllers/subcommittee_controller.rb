2ccfb0ae444be108545bc18c568f15af50c814c3
aneeshvaidya <aneesh.h.vaidya@gmail.com>
2016-04-15 16:18:42 +0000

    Partials win. Subcommittees all rendered in one template.

--- app/controllers/subcommittee_controller.rb
+++ app/controllers/subcommittee_controller.rb
@@ -3,9 +3,9 @@ class SubcommitteeController < ActionController::Base
     before_filter :authenticate_user!
     
     def index
-        @comittee_type = params[:committee_type]
-        @announcements = Announcement.where(committee_type: @comittee_type).order(created_at: :DESC)
-        @document_list = Document.where(committee_type: @comittee_type).order(created_at: :DESC)
+        @committee_type = params[:committee_type]
+        @announcements = Announcement.where(committee_type: @committee_type).order(created_at: :DESC)
+        @document_list = Document.where(committee_type: @committee_type).order(created_at: :DESC)
     end
     
 end
