fd16616b411580d812cf1d0c09b49a7413be1aa3
Zachary T. McPherson <zmcpher@berkeley.edu>
2016-04-16 05:57:15 +0000

    Pushig final changes

--- spec/controllers/subcommittee_controller_spec.rb
+++ spec/controllers/subcommittee_controller_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe SubcommitteeController, type: :controller do
+    fixtures :users
+    before(:each) do
+        sign_in users(:tester)
+    end
     describe 'index' do
        it 'renders index page' do
            get :subcommittee_index, {:committee_type => "executive"}
