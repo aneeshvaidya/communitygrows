c4234fa86ee556509f9bb0c861644480524b9484
Zachary T. McPherson <zmcpher@berkeley.edu>
2016-04-27 19:38:42 +0000

    rspec coverage to 94%

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -13,9 +13,14 @@ describe UserController do
         end
     end
     describe 'update' do
-        it 'should redirect to dashboard on success' do
+        it 'should redirect to account info page on success' do
             user_params = {:email => "tester@rspec.com", :password => "communitygrowsrocks", :password_confirmation => "communitygrowsrocks"}
-            put :update, user_id: users(:tester).id
+            put :update, user_id: users(:tester).id, :user => user_params
+            response.should redirect_to(update_user_credentials_path(users(:tester).id))
+        end
+        it 'should redirect to redirect to account info page on failure with failure message' do
+            user_params = {:email => "tester@rspec.com", :password => "", :password_confirmation => ""}
+            put :update, user_id: users(:tester).id, :user => user_params
             response.should redirect_to(update_user_credentials_path(users(:tester).id))
         end
     end
